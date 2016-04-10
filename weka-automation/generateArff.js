a6781eb642782deb912281f0db43f2feb0378d61
Miles Dowe <milesdowe@gmail.com>
2016-04-09 19:17:19 -0700

    Add initial automation script, test file

---
+++ weka-automation/generateArff.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Miles on 4/9/2016.
+ */
+require("shelljs/global");
+
+var arffLocation = '/c/Users/Miles/Documents/workspace/laugh-finder/python-training/wekaFile.arff';
+// var userOS = env.OS;
+
+// Open file
+// exec('java weka.core.Instances ' + arffLocation);
+
---
+++ weka-automation/tests/generateArff.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('test', function () {
+   it('1 should equal 1', function () {
+        assert.equal(1, 1);
+    });
+});
